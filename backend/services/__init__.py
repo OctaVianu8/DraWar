aaf96d7f449cfa011448dc92c35c463c856899d9
Octavianu <stanescu.matei.octavian@gmail.com>
2026-01-03 12:35:52 +0200

    start working on game manager service

---
+++ backend/services/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'WordGenerator', 'GameManager',
+    'AIServiceInterface',
+    'Prediction',
+    'get_ai_service',
+    'set_ai_service',
+    'RemoteAIService',
+    'ImageProcessor',
+    'image_processor',
+]
