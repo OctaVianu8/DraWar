aaa8670637622fc45463140c02d529ad2d471b56
Octavianu <stanescu.matei.octavian@gmail.com>
2026-01-03 16:31:31 +0200

    small fix

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,2 @@
 
-from backend.models.player import Player
-from backend.models.game import Game, GameState
-from backend.models.round import Round
-from backend.models.lobby import Lobby, LobbyState
-
 __all__ = ['Player', 'Game', 'GameState', 'Round', 'Lobby', 'LobbyState']
